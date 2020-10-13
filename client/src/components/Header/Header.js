d1535c8f78b6676e42fc1c52e21f23b71434dd2c
Erik Flores <erik.flores@colorado.edu>
2020-10-13 13:33:56 -0600

    Header
    Aligned Icons on Header and added Icons in images/icon folders.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import UserIcon from "./UserIcon";
+import Close from './Close'
+import newHeaderLogo from "../../assets/images/vetfest-logo.svg";
 
 const HeaderContainer = styled.div`
   padding: 0px 16px 0px 16px;
@@ -9,14 +12,23 @@ const HeaderContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   @media (min-width: 768px) {
-        min-height: 72px;
-    }
-`
+    min-height: 72px;
+  }
+`;
+
+// const LogoContainer = styled.div`
+//   min-height: 64px;
+//   width: 100%;
+//   background-image: url(${newHeaderLogo});
+//   background-repeat: no-repeat;
+// `;
 
 export default function Header() {
   return (
     <HeaderContainer>
-      Op Vets
+      <Close />
+      <img src={newHeaderLogo} alt={'O.P. Veterans'} />
+      <UserIcon />
     </HeaderContainer>
-  )
+  );
 }
---
+++ client/src/components/Header/UserIcon.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import avatar from '../../assets/icons/avatar-icon.svg'
+
+const IconContainer = styled.div`
+    margin: 0px 10px 0px 16px;
+    height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+    & > img {
+        width: 65px;
+        @media (min-width: 679px) {
+            display: flex;
+        }
+`
+
+
+export default function UserIcon() {
+    return (
+        <IconContainer>
+            <img src={avatar} alt={'My Account'} />
+        </IconContainer>
+    )
+}
