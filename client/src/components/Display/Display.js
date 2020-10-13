4b74fc9686511d0f11b4d9ae65a71bbcb0b8bbc6
Michael Stedman <michaelstedman81@gmail.com>
2020-10-13 17:19:38 -0600

    Adjusted Sizing

--- client/src/components/Display/Display.js
+++ client/src/components/Display/Display.js
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const DisplayContainer = styled.div`
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
     border: 2px dotted hotpink;
 `
 
