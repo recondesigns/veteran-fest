f238a966572cf84a847688d381059e1bb1896692
Michael Stedman <michaelstedman81@gmail.com>
2020-10-13 18:28:01 -0600

    Commit
    Commit to save work.

--- client/src/components/Display/Display.js
+++ client/src/components/Display/Display.js
@@ -10,7 +10,7 @@ const DisplayContainer = styled.div`
     box-sizing: border-box;
     width: 100%;
     height: 100%;
-    border: 2px dotted hotpink;
+    /* border: 2px dotted hotpink; */
 `
 
 export default function Display() {
@@ -23,7 +23,6 @@ export default function Display() {
 
     return (
         <DisplayContainer>
-            <p>Display Component</p>
             {display}
         </DisplayContainer>
     )
--- client/src/components/Display/Login.js
+++ client/src/components/Display/Login.js
@@ -1,14 +1,67 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Input } from '../Input'
+import { Button } from '../Button'
 
 const LoginContainer = styled.div`
-    border: 2px dashed purple;
+    /* width: 100%; */
+    height: 100%;
+    display: flex;
+    border: 1px solid purple;
 `
+
+const LeftPane = styled.div`
+    width: 520px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    border: 1px dotted blue;
+
+    & > p {
+        margin: 0px;
+        width: 100%;
+        font-family: Open Sans;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 12px;
+        line-height: 16px;
+        letter-spacing: 0.02em;
+        color: #545454;
+    }
+
+    & > h3 {
+        margin: 0px;
+        font-family: Helvetica Neue;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 28px;
+        line-height: 32px;
+        letter-spacing: 0.015em;
+        color: #545454;
+    }
+`
+
+const RightPane = styled.div`
+    width: 100%;
+    height: 100%;
+    background: lightblue;
+`
+
 
 export default function Login() {
     return (
         <LoginContainer>
-            <p>Login Component</p>
+            <LeftPane>
+                <p>Register with OP Veteran</p>
+                <h3>Create an account</h3>
+                <Input status={'default'} label={'Email'} />
+                <Input status={'default'} label={'Password'} />
+                <Button>Button</Button>
+                <Button>Button</Button>
+            </LeftPane>
+            <RightPane>
+                <Input status={'defaualt'} label={'Noooo'} />
+            </RightPane>
         </LoginContainer>
     )
 }
