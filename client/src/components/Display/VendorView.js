b4d362ec5dc2730a5ac0a8bbe283abb8bc0da05c
Michael Stedman <michaelstedman81@gmail.com>
2020-10-16 18:36:09 -0600

    Commit
    Commit

--- client/src/components/Display/VendorView.js
+++ client/src/components/Display/VendorView.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { StatusMessage } from '../StatusMessage'
 
 const VendorViewContainer = styled.div`
     /* border: 2px dashed purple; */
@@ -9,6 +10,7 @@ export default function VendorView() {
     return (
         <VendorViewContainer>
             <p>VendorView Component</p>
+            <StatusMessage status={'success'} message={'You have created an account.'} />
         </VendorViewContainer>
     )
 }
