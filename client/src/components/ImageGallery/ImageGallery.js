d9a48061d6cc898afd955b13c4e9d18efef8c58d
Michael Stedman <michaelstedman81@gmail.com>
2020-10-18 19:30:05 -0600

    Commit
    Commit

--- client/src/components/ImageGallery/ImageGallery.js
+++ client/src/components/ImageGallery/ImageGallery.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import Thumbnail from './Thumbnail'
 import MainImage from './MainImage'
 
+import { imageArr } from './utils'
+
 const ImageGalleryContainer = styled.div`
     box-sizing: border-box;
     width: 856px;
@@ -32,6 +34,17 @@ const ThumbnailWrapper = styled.div`
 `
 
 export default function ImageGallery() {
+    // console.log(imageArr)
+
+   let gallery = imageArr.map((imageObj, idx) => {
+       const { name, path } = imageObj
+    //    console.log(name, path)
+       return <Thumbnail key={idx} status={'active'} onClick={testClick} name={name} path={path} />
+   })
+
+   console.log(gallery)
+
+
     function testClick() {
         console.log('fired')
     }
@@ -40,6 +53,8 @@ export default function ImageGallery() {
         <ImageGalleryContainer>
             <Wrapper>
                 <ThumbnailWrapper>
+                    {gallery}
+                    {/* <Thumbnail status={'active'} onClick={testClick} />
                     <Thumbnail status={'active'} onClick={testClick} />
                     <Thumbnail status={'active'} onClick={testClick} />
                     <Thumbnail status={'active'} onClick={testClick} />
@@ -54,8 +69,7 @@ export default function ImageGallery() {
                     <Thumbnail status={'active'} onClick={testClick} />
                     <Thumbnail status={'active'} onClick={testClick} />
                     <Thumbnail status={'active'} onClick={testClick} />
-                    <Thumbnail status={'active'} onClick={testClick} />
-                    <Thumbnail status={'active'} onClick={testClick} />
+                    <Thumbnail status={'active'} onClick={testClick} /> */}
                 </ThumbnailWrapper>
             </Wrapper>
             <MainImage />
--- client/src/components/ImageGallery/utils.js
+++ client/src/components/ImageGallery/utils.js
@@ -1,3 +1,14 @@
+export const imageArr = [
+    {
+        name: 'vetFestImage01',
+        path: '../../assets/images/vetfest01-100px.jpg',
+    },
+    {
+        name: 'vetFestImage02',
+        path: '../../assets/images/vetfest02-100px.jpg',
+    }
+]
+
 export function setThumbnail(thumbnailStatus) {
     switch (thumbnailStatus) {
         case 'inactive':
