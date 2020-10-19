d2506a3a3436d1c7b4d093b3b5ed82d55b13fc93
Michael Stedman <michaelstedman81@gmail.com>
2020-10-19 12:54:13 -0600

    Updated MoreIcon.js
    MoreIcon is complete.

--- client/src/components/AdminCard/MoreIcon.js
+++ client/src/components/AdminCard/MoreIcon.js
@@ -6,24 +6,32 @@ import moreOpenIcon from '../../assets/icons/more-open-icon.svg'
 const MoreIconContainer = styled.div`
     width: 24px;
     height: 24px;
-    border: 1px dotted blue;
+    /* border: 1px dotted blue; */
 `
 
 export default function MoreIcon() {
     const [isIconOpen, setIsIconOpen] = useState(false)
 
-    function setIcon(status) {
-        if (status === false) {
+    function setIcon() {
+        if (isIconOpen === false) {
             return <img src={moreClosedIcon} alt={'Click to see more options.'} />
-        } else if (status === true) {
+        } else if (isIconOpen === true) {
             return <img src={moreOpenIcon} alt={'Click to see less options'} />
+        }
+    }
+
+    function changeIcon() {
+        if (isIconOpen === false) {
+            setIsIconOpen(true)
+        } else if (isIconOpen === true) {
+            setIsIconOpen(false)
         }
     }
 
     let iconToDisplay = setIcon(isIconOpen)
 
     return (
-        <MoreIconContainer>
+        <MoreIconContainer onClick={changeIcon}>
             {iconToDisplay}
         </MoreIconContainer>
     )
